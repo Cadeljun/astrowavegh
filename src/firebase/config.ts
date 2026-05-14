10e23a3fff4369e7d3b4d04ed1694e0356c9d268
Calvin (Cadel) <junioraquils143@gmail.com>
2026-05-14 18:17:56 +0000

    ## Error Type Runtime FirebaseError
    ## Error Message
    Firebase: Error (a

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyC_PLACEHOLDER_KEY",
+  authDomain: "astrowave-brand.firebaseapp.com",
+  projectId: "astrowave-brand",
+  storageBucket: "astrowave-brand.appspot.com",
+  messagingSenderId: "10987654321",
+  appId: "1:10987654321:web:abcdef123456789",
 };
