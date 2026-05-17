b0cdf3d5eb1bc61d13592f6f903ddd6c380bd54c
Calvin (Cadel) <junioraquils143@gmail.com>
2026-05-17 18:25:05 +0000

    https://res.cloudinary.com/dmd5bq3va/image/upload/v1779037411/astrowave/

--- src/lib/cms/definitions.ts
+++ src/lib/cms/definitions.ts
@@ -266,19 +266,19 @@ export interface GlobalSettings {
 export const DEFAULT_SETTINGS: GlobalSettings = {
   siteName: 'AstroWave',
   tagline: 'Vibes Beyond the Horizon.',
-  logoUrl: '',
+  logoUrl: 'https://res.cloudinary.com/dmd5bq3va/image/upload/v1779037411/astrowave/brand/logos/exh6fktxl59ixntyaqnz.jpg',
   email: 'info@astrowave.com',
   location: 'Accra, Ghana',
-  instagram: '',
-  twitter: '',
+  instagram: 'https://instagram.com/astrowavegh',
+  twitter: 'https://twitter.com/astrowavegh',
   tiktok: '',
   youtube: '',
   facebook: '',
   heroVideoUrl: '',
-  heroPosterUrl: '',
-  heroImageUrl: '',
-  founderImageUrl: '',
-  defaultEventPoster: '',
-  defaultTalentPhoto: '',
-  defaultGalleryPhoto: ''
+  heroPosterUrl: 'https://images.unsplash.com/photo-1492684223066-81342ee5ff30?q=80&w=1200',
+  heroImageUrl: 'https://images.unsplash.com/photo-1492684223066-81342ee5ff30?q=80&w=1200',
+  founderImageUrl: 'https://images.unsplash.com/photo-1508700115892-45ecd05ae2ad?q=80&w=400',
+  defaultEventPoster: 'https://images.unsplash.com/photo-1514525253361-bee8a187449b?q=80&w=800',
+  defaultTalentPhoto: 'https://images.unsplash.com/photo-1508700115892-45ecd05ae2ad?q=80&w=400',
+  defaultGalleryPhoto: 'https://images.unsplash.com/photo-1533174072545-7a4b6ad7a6c3?q=80&w=800'
 }
